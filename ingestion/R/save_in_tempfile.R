d340ab763c2af552340d98211ce777894a0b62e4
Fernando Cagua <efcaguab@gmail.com>
2020-10-26 18:09:29 +1300

    Fix error in function

--- ingestion/R/save_in_tempfile.R
+++ ingestion/R/save_in_tempfile.R
@@ -3,6 +3,6 @@ save_in_tempfile <- function(content, prefix, extension){
   file_name <- paste0(prefix, "_",content_date, ".", extension)
   dir_name <- tempdir()
   path <- file.path(dir_name, file_name)
-  writeLines(text = retrieved_data, con = path)
+  writeLines(text = content, con = path)
   path
 }
