45a7947f8fe22b3283236d710221428f86477e79
Fernando Cagua <efcaguab@gmail.com>
2020-10-26 16:19:44 +1300

    FInish off API to update to bigquery

--- validation/api.R
+++ validation/api.R
@@ -42,15 +42,57 @@ validate_data <- function(x){
 
   cat("Data parameters:", as.character(list(data = x)), "\n")
 
-  # Build URI
-  this_dataset$source_uri <- paste0("gs://", x$bucket, "/", x$name)
-  this_dataset$name <- x$name
+  upload_to_bq(
+    object = x$name,
+    bucket = x$bucket,
+    project = params$project$id,
+    object_col = params$validation$bigquery$raw$coltypes,
+    bq_dataset = params$validation$bigquery$raw$dataset,
+    bq_table = params$validation$bigquery$raw$table,
+    bq_location = params$validation$bigquery$raw$location,
+    bq_write_disposition = params$validation$bigquery$raw$write_disposition,
+    bq_create_disposition = params$validation$bigquery$raw$create_disposition,
+    secret_path = params$secret$file)
 
-  upload_to_bq()
-
-  return("hello world")
+  return(0)
 }
 
-upload_to_bq <- function(){
-  bigrquery::bq_auth(path = params$secret$file)
+upload_to_bq <- function(object, bucket, project, object_col, bq_dataset, bq_table,
+                         bq_location, bq_write_disposition, bq_create_disposition,
+                         secret_path){
+  bigrquery::bq_auth(path = secret_path)
+
+  # Check if dataset exists
+  dataset_bq <- bigrquery::bq_dataset(project = project, dataset = bq_dataset)
+  if (!bigrquery::bq_dataset_exists(dataset_bq)) {
+    bigrquery::bq_dataset_create(x = dataset_bq, location = bq_location)
+  }
+
+  values <- get_raw_csv(object, bucket, object_col, secret_path)
+  table_bq <- bigrquery::bq_table(project, bq_dataset, bq_table)
+
+  bigrquery::bq_table_upload(
+    table_bq,
+    values,
+    write_disposition = bq_write_disposition,
+    create_disposition = bq_create_disposition)
+
+}
+
+get_raw_csv <- function(object, bucket, object_col, secret_path){
+  googleCloudStorageR::gcs_auth(json_file = secret_path)
+
+  # Download csv data
+  values_path <- tempfile(fileext = ".csv")
+  values_path <- basename(values_path)
+  # Make sure file is deleted
+  on.exit(file.remove(values_path))
+
+  googleCloudStorageR::gcs_get_object(object_name = object,
+                                      bucket = bucket,
+                                      saveToDisk = values_path)
+  # Clean column names
+  values <- readr::read_csv(values_path, guess_max = 10000,
+                            col_types = object_col)
+  janitor::clean_names(values)
 }
